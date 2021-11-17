308fd85815653836908d821a6b87ba0e2c8c15a8
brandon <brandonalexis845@gmail.com>
2021-11-17 17:51:31 -0600

    modelo

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    notes = models.CharField(max_length=500)
+    dosis = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
